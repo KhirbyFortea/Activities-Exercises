d81c41015764e1001035e38329711a14dadba038
KhirbyFortea <35440633+KhirbyFortea@users.noreply.github.com>
2018-04-04 21:22:26 +0800

    Add files via upload

---
+++ BubbleSort-Fortea.py
@@ -0,0 +1,11 @@
+def bubbleSort(nlist):
+    for passnum in range(len(nlist)-1,0,-1):
+        for i in range(passnum):
+            if nlist[i]>nlist[i+1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i+1]
+                nlist[i+1] = temp
+
+nlist = [54,65,1,77,32,41,99,21,14]
+bubbleSort(nlist)
+print(nlist)
