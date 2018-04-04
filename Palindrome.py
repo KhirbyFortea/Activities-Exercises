b6131ec664a9149ba83f4a64752be5e83d66d320
KhirbyFortea <35440633+KhirbyFortea@users.noreply.github.com>
2018-04-04 18:12:19 +0800

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,15 @@
+#PAUL KHIRBY B. FORTEA
+#github.com/KhirbyFortea
+#Khirby.Fortea@gmail.com
+#Palindrome
+
+
+
+def isPalindrome():
+    string = input('Enter a string: ')
+    string1 = string[::-1]
+    if string[0] == string[(len(string)-1)] and string[1:(len(string)-2)] == string1[1:(len(string)-2)]:
+            print('It is a palindrome')
+    else:
+        print('It is not a palindrome')
+isPalindrome()
